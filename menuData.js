415d9d9af00184b25a7462107999a57da7f38206
kamal11111111 <berikutny779@gmail.com>
2026-05-23 10:28:31 +0700

    Update menuData.js

---
+++ menuData.js
@@ -0,0 +1,1451 @@
+// =======================
+// DATASET MBG
+// AUTO GENERATED
+// =======================
+
+const menuBasah=[];
+
+const menuKering=[];
+
+const tambahan=[];
+
+
+
+// =======================
+// HELPER
+// =======================
+
+function tambahMenu(
+target,
+nama,
+isi,
+energi,
+protein,
+lemak,
+karbo,
+serat,
+biaya
+){
+
+target.push({
+
+nama,
+isi,
+energi,
+protein,
+lemak,
+karbo,
+serat,
+biaya
+
+});
+
+}
+
+
+
+function tambahTambahan(
+
+nama,
+kategori,
+harga,
+energi,
+protein,
+lemak,
+karbo,
+serat
+
+){
+
+tambahan.push({
+
+nama,
+kategori,
+harga,
+energi,
+protein,
+lemak,
+karbo,
+serat
+
+});
+
+}
+
+
+
+// =======================
+// MENU BASAH (BATCH 1)
+// =======================
+
+tambahMenu(
+menuBasah,
+"Menu Basah 1",
+[
+"Nasi putih",
+"Dori krispi",
+"Sayur lodeh",
+"Tempe goreng",
+"Kurma"
+],
+774.9,
+30.8,
+18.8,
+124.5,
+6.2,
+10000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 2",
+[
+"Nasi putih",
+"Martabak ayam",
+"Dori asam manis",
+"Sup oyong wortel",
+"Kurma"
+],
+631.3,
+25.2,
+10.4,
+98.9,
+4.1,
+10000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 3",
+[
+"Nasi putih",
+"Tahu bakso",
+"Telur pontianak",
+"Sayur lebaran",
+"Susu pasteurisasi"
+],
+660.5,
+19.4,
+14.2,
+106.8,
+4.6,
+10000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 4",
+[
+"Nasi putih",
+"Dori sambal matah",
+"Sup macaroni",
+"Mendoan",
+"Buah jeruk"
+],
+730.2,
+28.4,
+18.8,
+113.5,
+4.3,
+10000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 5",
+[
+"Nasi putih",
+"Tempe goreng",
+"Tumis buncis bakso",
+"Telur pindang",
+"Semangka"
+],
+719.1,
+30.1,
+23.4,
+98.3,
+3.8,
+10000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 6",
+[
+"Nasi putih",
+"Tahu sumedang",
+"Sup pelangi",
+"Dori lemon butter",
+"Kurma"
+],
+868.6,
+25,
+15.7,
+164.2,
+14.9,
+10000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 7",
+[
+"Nasi putih",
+"Ayam srundeng",
+"Gulai kapau",
+"Tahu goreng",
+"Semangka"
+],
+626.9,
+32,
+12.9,
+94.5,
+2.4,
+10000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 8",
+[
+"Nasi putih",
+"Kering tempe",
+"Buncis XO",
+"Telur dadar padang",
+"Buah pisang"
+],
+725.3,
+27.3,
+19.8,
+112.7,
+7.4,
+9500
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 9",
+[
+"Nasi putih",
+"Tumis labu siam",
+"Tahu sumedang",
+"Otak-otak bandeng goreng",
+"Buah jeruk"
+],
+587.4,
+18.9,
+14,
+95.8,
+5,
+9500
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 10",
+[
+"Nasi putih",
+"Tempe krispi",
+"Ayam goreng kalasan",
+"Asem-asem buncis",
+"Buah semangka"
+],
+693.3,
+36.8,
+16.1,
+100.7,
+3,
+10000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 11",
+[
+"Nasi putih",
+"Gudangan",
+"Tahu bakso",
+"Telur bumbu bali",
+"Melon madu"
+],
+579,
+17.9,
+10.9,
+100.1,
+3.6,
+10000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 12",
+[
+"Nasi liwet",
+"Lalapan & sambal hijau",
+"Tempe goreng",
+"Nila goreng krispi",
+"Jeruk baby"
+],
+656.1,
+35.3,
+16,
+92.3,
+4.2,
+13000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 13",
+[
+"Nasi putih",
+"Salad sayur",
+"Chicken katsu",
+"Tahu cabai garam",
+"Jeruk medan"
+],
+680.1,
+32.7,
+12.7,
+107.8,
+2.8,
+10000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 14",
+[
+"Mie yamin",
+"Ayam kecap",
+"Pangsit",
+"Sawi & acar",
+"Saos sambal",
+"Buah pisang"
+],
+644.2,
+30,
+12.9,
+105.8,
+8.8,
+10000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 15",
+[
+"Nasi putih",
+"Orak-arik sayur",
+"Sapi lada hitam",
+"Tahu goreng",
+"Buah naga"
+],
+650,
+25.9,
+17.3,
+96.2,
+4.9,
+11500
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 16",
+[
+"Nasi kuning",
+"Orek tempe",
+"Acar matang",
+"Telur balado",
+"Semangka"
+],
+714.8,
+29.2,
+23.8,
+97.1,
+4.2,
+9000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 17",
+[
+"Nasi putih",
+"Tahu sumedang",
+"Ayam bakar",
+"Lalapan & sambal bajak",
+"Jeruk baby"
+],
+613.7,
+21.9,
+17.9,
+90.3,
+2.4,
+10000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 18",
+[
+"Nasi goreng XO",
+"Acar & wortel",
+"Tahu bakso",
+"Telur ceplok",
+"Susu pasteurisasi"
+],
+646.8,
+20,
+29.2,
+72.1,
+2.5,
+10000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 19",
+[
+"Nasi putih",
+"Tahu goreng",
+"Ayam rempah",
+"Sayur sup",
+"Kelengkeng"
+],
+747.1,
+29.6,
+26.7,
+99.1,
+4.2,
+10000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 20",
+[
+"Nasi putih",
+"Bakso goreng",
+"Ayam bumbu kemangi",
+"Tumis buncis wortel",
+"Buah naga"
+],
+643.2,
+36,
+10.8,
+96.9,
+5.2,
+10000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 21",
+[
+"Kentang rebus",
+"Telur rebus",
+"Kol",
+"Tahu rebus",
+"Dimsum mentai",
+"Sambal kacang",
+"Kelengkeng"
+],
+495.3,
+25.5,
+25.6,
+42.6,
+2.6,
+11000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 22",
+[
+"Nasi putih",
+"Ayam katsu",
+"Tahu krispi",
+"Sup cream jagung",
+"Susu pasteurisasi"
+],
+715.7,
+33.6,
+11.7,
+112.2,
+0.7,
+11000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 23",
+[
+"Nasi putih",
+"Lele goreng",
+"Tahu bakso",
+"Lalapan & sambal bajak",
+"Jeruk baby"
+],
+553.6,
+22.1,
+23.3,
+64.5,
+4.2,
+10000
+);
+
+
+
+tambahMenu(
+menuBasah,
+"Menu Basah 24",
+[
+"Mie jawa",
+"Telur ceplok",
+"Acar (timun wortel)",
+"Pangsit",
+"Buah semangka"
+],
+621.6,
+23.1,
+28.5,
+68.4,
+1.8,
+9500
+);
+
+tambahMenu(
+menuBasah,
+"Menu Basah 25",
+[
+"Nasi putih",
+"Chicken nugget",
+"Tahu bakso",
+"Mix vegetables",
+"Jeruk baby"
+],
+656.7,
+31.5,
+9.9,
+108.9,
+2.7,
+10000
+);
+
+tambahMenu(
+menuBasah,
+"Menu Basah 26",
+[
+"Nasi putih",
+"Orak-arik sayur",
+"Ayam kungpao",
+"Tahu goreng",
+"Buah pisang"
+],
+702.8,
+33.6,
+11.2,
+117.6,
+6.8,
+10000
+);
+
+tambahMenu(
+menuBasah,
+"Menu Basah 27",
+[
+"Nasi jamblang",
+"Ayam goreng bawang putih",
+"Lalapan",
+"Sambal kecombrang",
+"Jeruk pontianak"
+],
+701.2,
+38.3,
+18.6,
+95.4,
+5.5,
+10000
+);
+
+tambahMenu(
+menuBasah,
+"Menu Basah 28",
+[
+"Nasi putih",
+"Tahu goreng",
+"Kuah sayur sup",
+"Kentang & wortel",
+"Iga sapi panggang",
+"Kelengkeng"
+],
+598.8,
+20.5,
+14.2,
+96.3,
+1.5,
+12500
+);
+
+tambahMenu(
+menuBasah,
+"Menu Basah 29",
+[
+"Nasi putih",
+"Tahu bacem",
+"Telur goreng",
+"Sayur asem",
+"Jeruk baby"
+],
+565.4,
+19.7,
+11.3,
+95,
+2.7,
+9000
+);
+
+tambahMenu(
+menuBasah,
+"Menu Basah 30",
+[
+"Nasi putih",
+"Tumis buncis putren bakso",
+"Ayam balado",
+"Tahu goreng",
+"Semangka"
+],
+767.4,
+40.8,
+19.9,
+106,
+3.4,
+10000
+);
+
+tambahMenu(
+menuBasah,
+"Menu Basah 31",
+[
+"Nasi putih",
+"Tahu bakso",
+"Sayur nangka muda",
+"Telur bacem",
+"Buah pisang"
+],
+676.7,
+19.5,
+10.7,
+126.3,
+10.7,
+9500
+);
+
+tambahMenu(
+menuBasah,
+"Menu Basah 32",
+[
+"Nasi putih",
+"Kering tempe",
+"Tumis labu siam",
+"Terik ayam",
+"Buah jeruk"
+],
+758.2,
+29.7,
+26.9,
+101.2,
+7,
+10000
+);
+
+tambahMenu(
+menuBasah,
+"Menu Basah 33",
+[
+"Nasi putih",
+"Tahu goreng",
+"Telur goreng",
+"Tumis buncis XO",
+"Susu pasteurisasi"
+],
+609.5,
+20.2,
+12.4,
+98.4,
+1.2,
+9500
+);
+
+tambahMenu(
+menuBasah,
+"Menu Basah 34",
+[
+"Rainbow rice",
+"Acar",
+"Chicken curry",
+"Rolade",
+"Buah semangka"
+],
+649.3,
+22.5,
+20.9,
+92.7,
+1.5,
+10500
+);
+
+tambahMenu(
+menuBasah,
+"Menu Basah 35",
+[
+"Nasi putih",
+"Kering tempe",
+"Tumis buncis wortel",
+"Telur pontianak",
+"Jeruk baby"
+],
+734.7,
+27.9,
+20,
+113.9,
+6.9,
+9000
+);
+
+
+
+// =======================
+// MENU KERING
+// =======================
+tambahMenu(
+menuKering,
+"Menu Kering 1",
+[
+"Cocktail buah",
+"Telur rebus",
+"Kurma",
+"Macaroni scottel"
+],
+539.6,
+17.2,
+18.3,
+79.4,
+5.9,
+10000
+);
+
+tambahMenu(
+menuKering,
+"Menu Kering 2",
+[
+"Kroket ayam",
+"Telur rebus",
+"Buah pisang",
+"Kurma",
+"Es kuwut"
+],
+606.4,
+14.8,
+27.7,
+81.2,
+8.2,
+10000
+);
+
+tambahMenu(
+menuKering,
+"Menu Kering 3",
+[
+"Pisang rebus",
+"Jagung rebus",
+"Ubi cilembu",
+"Telur rebus",
+"Susu pasteurisasi"
+],
+606.4,
+14.8,
+27.7,
+81.2,
+8.2,
+10000
+);
+
+tambahMenu(
+menuKering,
+"Menu Kering 4",
+[
+"Martabak ayam",
+"Kurma",
+"Singkong thailand",
+"Pastel ayam",
+"Buah salak"
+],
+454.2,
+12.2,
+11.6,
+76.3,
+4.3,
+10000
+);
+
+tambahMenu(
+menuKering,
+"Menu Kering 5",
+[
+"Tahu bakso",
+"Telur rebus",
+"Dimsum",
+"Jagung rebus",
+"Sambal kacang",
+"Buah jeruk"
+],
+496.9,
+25.7,
+25.9,
+45.4,
+3.6,
+10000
+);
+
+tambahMenu(
+menuKering,
+"Menu Kering 6",
+[
+"Lumpia",
+"Telur rebus",
+"Tahu fantasi",
+"Kurma",
+"Buah pisang"
+],
+407.9,
+16.7,
+13.9,
+61.2,
+8.9,
+10000
+);
+
+tambahMenu(
+menuKering,
+"Menu Kering 7",
+[
+"Timus cilembu",
+"Kue ku",
+"Susu pasteurisasi",
+"Lemper abon",
+"Buah pisang"
+],
+542.9,
+18.9,
+19,
+72.7,
+8.6,
+10000
+);
+
+tambahMenu(
+menuKering,
+"Menu Kering 8",
+[
+"Chiffon cake",
+"Kurma",
+"Jus buah naga",
+"Sosis solo",
+"Buah jeruk"
+],
+394.7,
+10.6,
+12.8,
+62.6,
+8.7,
+10000
+);
+
+tambahMenu(
+menuKering,
+"Menu Kering 9",
+[
+"Risol rogut",
+"Kurma",
+"Susu pasteurisasi",
+"Telur rebus",
+"Misoa goreng"
+],
+517,
+15.5,
+13,
+80.8,
+4.1,
+10000
+);
+
+tambahMenu(
+menuKering,
+"Menu Kering 10",
+[
+"Risol mayo",
+"Bolu mandarin",
+"Jagung keju",
+"Onde-onde",
+"Buah jeruk"
+],
+623.8,
+12.8,
+22.4,
+96.2,
+2.9,
+10000
+);
+
+tambahMenu(
+menuKering,
+"Menu Kering 11",
+[
+"Telur rebus",
+"Ayam goreng lengkuas",
+"Jeruk baby",
+"Pisang goreng wijen",
+"Bolu gulung"
+],
+598.5,
+30.6,
+22.6,
+67,
+7.1,
+11000
+);
+
+tambahMenu(
+menuKering,
+"Menu Kering 12",
+[
+"Jus melon",
+"Lumpia",
+"Mochi",
+"Tempe bacem",
+"Ayam bacem"
+],
+490.4,
+25.9,
+19.9,
+53.3,
+2.8,
+10000
+);
+
+tambahMenu(
+menuKering,
+"Menu Kering 13",
+[
+"Bubur mutiara",
+"Kue bugis",
+"Buah pisang",
+"Tahu bakso",
+"Jagung keju"
+],
+538,
+14.4,
+24.6,
+67.8,
+1.8,
+10000
+);
+
+tambahMenu(
+menuKering,
+"Menu Kering 14",
+[
+"Cookies daun kelor",
+"Wingko babat",
+"Telur rebus",
+"Buah pisang",
+"Apel fuji",
+"Dimsum goreng",
+"Kurma"
+],
+642.7,
+19.4,
+17.3,
+109.2,
+13.5,
+19000
+);
+
+tambahMenu(
+menuKering,
+"Menu Kering 15",
+[
+"Donat keju",
+"Telur bebek",
+"Tahu bakso",
+"Jeruk baby",
+"Kurma",
+"Telur rebus",
+"Molen"
+],
+570,
+21.4,
+23.9,
+70.9,
+5.8,
+16500
+);
+
+tambahMenu(
+menuKering,
+"Menu Kering 16",
+[
+"Ayam ungkep",
+"Tempe ungkep",
+"Tahu bakso",
+"Lumpia",
+"Kurma",
+"Buah pir",
+"Buah jeruk"
+],
+635.5,
+27.7,
+23.8,
+88.3,
+13.8,
+17000
+);
+
+tambahMenu(
+menuKering,
+"Menu Kering 17",
+[
+"Tempe ungkep",
+"Tahu ungkep",
+"Ayam ungkep",
+"Kue lapis",
+"Buah pisang"
+],
+1019.5,
+40.1,
+42.8,
+130.2,
+22.5,
+10000
+);
+
+tambahMenu(
+menuKering,
+"Menu Kering 18",
+[
+"Telur rebus",
+"Telur bebek",
+"Buah jeruk",
+"Buah naga"
+],
+698.5,
+36.4,
+31.4,
+75.5,
+16.3,
+13000
+);
+
+
+
+// =======================
+// ITEM TAMBAHAN
+// =======================
+
+tambahTambahan(
+"Susu kedelai",
+"Protein",
+5000,
+80,
+9,
+4,
+10,
+0
+);
+
+tambahTambahan(
+"Nugget ikan so good",
+"Protein",
+5000,
+76,
+3.6,
+0.68,
+14.4,
+0
+);
+
+tambahTambahan(
+"Edamame rebus superindo",
+"Protein",
+4500,
+73,
+6.6,
+3,
+6,
+3
+);
+
+tambahTambahan(
+"Sate telur puyuh",
+"Protein",
+3000,
+79,
+13.05,
+5.55,
+0.2,
+0
+);
+
+tambahTambahan(
+"Fish cake cedea",
+"Protein",
+3000,
+100,
+4,
+6,
+8,
+0
+);
+
+tambahTambahan(
+"Bubur ketan hitam",
+"Protein",
+3000,
+120.5,
+1.485,
+2.695,
+22.685,
+0.75
+);
+
+tambahTambahan(
+"Cimory yogurt stick original",
+"Protein",
+2500,
+45,
+1,
+1.5,
+7,
+1
+);
+
+tambahTambahan(
+"Keju slice",
+"Protein",
+1500,
+50,
+1,
+4.5,
+3,
+0
+);
+
+tambahTambahan(
+"Chia pudding mini",
+"Lemak Sehat",
+8000,
+113,
+3.3,
+4.98,
+15.03,
+4.4
+);
+
+tambahTambahan(
+"Kacang almond mini pack",
+"Lemak Sehat",
+5000,
+180,
+8,
+15,
+3,
+3
+);
+
+tambahTambahan(
+"Puding alpukat",
+"Lemak Sehat",
+4500,
+335,
+2.58,
+9.03,
+64.52,
+0
+);
+
+tambahTambahan(
+"Susu oat nobo",
+"Lemak Sehat",
+3500,
+118,
+2,
+6,
+13.2,
+1.6
+);
+
+tambahTambahan(
+"Kacang tanah rebus",
+"Lemak Sehat",
+3000,
+300,
+14.02,
+26.25,
+7.63,
+4.7
+);
+
+tambahTambahan(
+"Biji bunga matahari mini",
+"Lemak Sehat",
+3000,
+116,
+3.87,
+9.96,
+4.81,
+2.2
+);
+
+tambahTambahan(
+"Alpukat cup",
+"Lemak Sehat",
+2500,
+128,
+1.6,
+11.73,
+6.82,
+5.4
+);
+
+tambahTambahan(
+"Kelapa muda cup",
+"Lemak Sehat",
+2500,
+58,
+0.11,
+1.05,
+12.5,
+0.1
+);
+
+tambahTambahan(
+"Oat bar",
+"Serat",
+5500,
+175,
+1.94,
+7.78,
+21.39,
+3.9
+);
+
+tambahTambahan(
+"Bubur kacang hijau",
+"Serat",
+5000,
+162,
+5.3,
+4.6,
+26.64,
+4.3
+);
+
+tambahTambahan(
+"Jagung susu mini",
+"Serat",
+5000,
+197,
+5.02,
+2.47,
+39.72,
+1
+);
+
+tambahTambahan(
+"Rumput laut",
+"Serat",
+5000,
+30,
+1,
+2.5,
+1,
+1
+);
+
+tambahTambahan(
+"Brokoli rebus",
+"Serat",
+4000,
+43,
+1.84,
+2.15,
+5.58,
+2.6
+);
+
+tambahTambahan(
+"Buah pir",
+"Serat",
+3500,
+58,
+0.38,
+0.12,
+15.46,
+3.1
+);
+
+tambahTambahan(
+"Kurma",
+"Serat",
+3500,
+99,
+0.86,
+0.14,
+26.26,
+2.8
+);
+
+tambahTambahan(
+"Jagung rebus cup",
+"Serat",
+3000,
+93,
+3.11,
+0.74,
+22.33,
+2.8
+);
+
+tambahTambahan(
+"Ubi ungu kukus",
+"Serat",
+3000,
+82,
+2.36,
+0.05,
+18.26,
+2.7
+);
+
+tambahTambahan(
+"Roti gandum mini",
+"Serat",
+2500,
+95,
+3,
+1.75,
+16.5,
+3
+);
+
+tambahTambahan(
+"Salad buah cup",
+"Serat",
+2500,
+68,
+0.8,
+1.03,
+15.79,
+2.1
+);
+
+tambahTambahan(
+"Pisang cavendish",
+"Serat",
+2500,
+66,
+0.75,
+0,
+16.5,
+1.5
+);
+
+tambahTambahan(
+"Buah naga",
+"Serat",
+2500,
+41,
+0.63,
+0.3,
+9.9,
+1.4
+);
+
+tambahTambahan(
+"Salad sayur mini",
+"Serat",
+2500,
+32,
+2.24,
+1.64,
+2.18,
+0.7
+);
+
+tambahTambahan(
+"Apel mini",
+"Serat",
+2000,
+47,
+0.23,
+0.15,
+12.43,
+2.2
+);
+
+tambahTambahan(
+"Biskuit gandum",
+"Serat",
+1500,
+92,
+2.05,
+4.1,
+13.33,
+1.5
+);
